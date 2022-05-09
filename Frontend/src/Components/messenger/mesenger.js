1eca67a07d5927888ad8d204933fb433e3a01d23
hiwot <hiwotzelalem3@gmal.com>
2022-05-09 22:12:40 +0300

    modified chat

--- Frontend/src/Components/messenger/mesenger.js
+++ Frontend/src/Components/messenger/mesenger.js
@@ -5,14 +5,58 @@ export default function mesenger() {
   return (
 <>
       <Navbar />
-    <div className="messenger">
-        
-        <div className="chatMenu">Menu</div>
-        <div className="chatBox">Box</div>
-        <div className="chatOnline">online</div> 
-    </div>  
-      
-</>
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input placeholder="Search for friends" className="chatMenuInput" />
+            {/* {conversations.map((c) => (
+              <div onClick={() => setCurrentChat(c)}>
+                <Conversation conversation={c} currentUser={user} />
+              </div>
+            ))} */}
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            {/* {currentChat ? (
+              <>
+                <div className="chatBoxTop">
+                  {messages.map((m) => (
+                    <div ref={scrollRef}>
+                      <Message message={m} own={m.sender === user._id} />
+                    </div>
+                  ))}
+                </div>
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="write something..."
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button className="chatSubmitButton" onClick={handleSubmit}>
+                    Send
+                  </button>
+                </div>
+              </>
+            ) : (
+              <span className="noConversationText">
+                Open a conversation to start a chat.
+              </span>
+            )} */}
+          </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">
+            {/* <ChatOnline
+              onlineUsers={onlineUsers}
+              currentId={user._id}
+              setCurrentChat={setCurrentChat}
+            /> */}
+          </div>
+        </div>
+      </div>
+    </>
       
   )
 }
